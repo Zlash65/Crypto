d95ba8a820b3c8354f74a7830617e14b1401e09d
Zlash65 <zarrar65@gmail.com>
2018-06-10 00:24:17 +0530

    desktop updated

--- crypto/config/crypto.py
+++ crypto/config/crypto.py
@@ -4,12 +4,41 @@ from frappe import _
 def get_data():
 	return [
 		{
+			"label": _("User"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "User Profile",
+					"description": _("User data.")
+				},
+				{
+					"type": "doctype",
+					"name": "My Investments",
+					"description": _("Keep track of invested coins.")
+				},
+				{
+					"type": "report",
+					"name": "My Investments Report",
+					"doctype": "My Investments",
+					"description": _("Arbitrage coin data pertaining to User's investments"),
+					"is_query_report": True
+				}
+			]
+		},
+		{
 			"label": _("Exchange"),
 			"items": [
 				{
 					"type": "doctype",
 					"name": "Coin Exchange",
 					"description": _("Data fetched from each market.")
+				},
+				{
+					"type": "report",
+					"name": "Arbitrage Report",
+					"doctype": "Coin Exchange",
+					"description": _("Overall Arbitrage data"),
+					"is_query_report": True
 				}
 			]
 		},
@@ -25,6 +54,11 @@ def get_data():
 					"type": "doctype",
 					"name": "Market",
 					"description": _("Market detail and fees for each coin.")
+				},
+				{
+					"type": "doctype",
+					"name": "Market API",
+					"description": _("Setup API's for market added.")
 				}
 			]
 		}
